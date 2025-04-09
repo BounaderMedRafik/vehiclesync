9e2ee0423f60d89f26c9996be3f1d3ad7958a1b7
William Rice <william@williamarice.com>
2025-04-09 15:28:09 -0400

    fix failed build for testing pipeline

--- src/components/home.test.tsx
+++ src/components/home.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 
-import { fireEvent, render, screen } from "@testing-library/react";
+// import { fireEvent, render, screen } from "@testing-library/react";
 
 function sum(a: number, b: number): number {
   return a + b;
