ff54ae51740dcf7d2377ca298d026655e3d61a1d
William Rice <william@williamarice.com>
2025-04-09 21:07:12 -0400

    Testing: testing fully configured

---
+++ __tests__/dummy.test.ts
@@ -0,0 +1,7 @@
+// This is a dummy test for CI/CD build early development. Will delete later.
+
+describe("dummy test", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-export default async function Home() {
+export default async function HomePage() {
   return (
     <div className="size-full flex flex-col gap-8 items-center justify-center">
       Home
