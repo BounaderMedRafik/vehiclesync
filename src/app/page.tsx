1afb062d064f4ead07e8ac47edadc3f67f691295
William Rice <william@williamarice.com>
2025-05-03 22:27:40 -0400

    fixed bug to redirect users to the dashboard page if they are still logged in with a session and return to the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import LandingPageInfoBlock from "@/components/LandingPageInfoBlock";
 import LandingPageLoginBlock from "@/components/LandingPageLoginBlock";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function HomePage() {
+  const session = await auth();
+
+  if (session?.user) {
+    return redirect("/dashboard");
+  }
+
   return (
     <div className="w-full h-[100vh] flex items-center justify-center bg-[url('/landing_page_bg.jpg')] bg-cover bg-no-repeat bg-center">
       <div className="flex min-w-3xl items-center justify-center flex1 h-96">
